892032b2ed44007151c428a03ddb13f63bc4bceb
Higor Turetta <higorturetta@hotmail.com>
2020-11-11 14:38:30 -0300

    page sobreNos created

---
+++ site/sobreNos.php
@@ -0,0 +1,24 @@
+<section class="sobreNos-section Padrao">
+    <div class="sobreNos-title">
+        <h2 class="text-fluid">Conheça um pouco mais sobre nós!</h2>
+    </div>
+    <div class="container">
+        <div class="row sobreNos-description">
+            <div class="col-lg-4 ">
+                <img src="<?= SITE_URL . "assets/images/SaraPNG" ?>" alt="Oi, Sou a Sara!">
+            </div>
+            <div class="col-lg-8">
+                <p class="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget porta est,
+                    sed vulputate libero. Duis ornare est nibh, a tristique metus mattis a. Mauris volutpat sapien
+                    eget lectus tristique, at posuere purus consequat. Nulla interdum orci non nibh tempus,
+                    ac ullamcorper lectus pellentesque. Nulla sed pellentesque elit. Vestibulum sit amet fermentum leo.
+                    Morbi sed est placerat, eleifend ipsum quis, rutrum massa. Cras non pretium magna.
+                    Pellentesque risus nisl, vulputate id metus eget, luctus mollis risus.
+                    Duis quis augue elit. Nulla convallis lectus eget ultrices luctus.
+                    Aliquam et magna sodales, blandit dui eu, accumsan lacus. Curabitur vel turpis nunc.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
