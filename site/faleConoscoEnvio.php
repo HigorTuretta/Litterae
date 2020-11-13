5b6176290e016ee326a7ee0f64891f37b2c874b2
Higor Turetta <higorturetta@hotmail.com>
2020-11-13 20:22:54 -0300

    faleConoscoUpdate

--- site/faleConoscoEnvio.php
+++ site/faleConoscoEnvio.php
@@ -11,8 +11,8 @@ use PHPMailer\PHPMailer\Exception;
 require_once 'assets/util/phpmailer/vendor/autoload.php';
 $TipoProjeto = "";
 
-if($_POST['TipoProjeto'] == 1 ? $TipoProjeto =  "Parede Personalizada" : ($_POST['TipoProjeto'] == 2 ? $TipoProjeto = "Quadro Personalizado": ($_POST['TipoProjeto'] == 3 ? $TipoProjeto = "Lettering para publicidade" : $TipoProjeto = "Lettering Geral")));
-$mensagem = "Orçamento para " . $TipoProjeto . "<br><br> <p>" . $_POST['Descricao'] ."</p><br>Pedido Realizado por: " . $_POST['Nome']. "<br>Email: " . $_POST['Email']; 
+if ($_POST['TipoProjeto'] == 1 ? $TipoProjeto =  "Parede Personalizada" : ($_POST['TipoProjeto'] == 2 ? $TipoProjeto = "Quadro Personalizado" : ($_POST['TipoProjeto'] == 3 ? $TipoProjeto = "Lettering para publicidade" : $TipoProjeto = "Lettering Geral")));
+$mensagem = "Orçamento para " . $TipoProjeto . "<br><br> <p>" . $_POST['Descricao'] . "</p><br>Pedido Realizado por: " . $_POST['Nome'] . "<br>Email: <a href= mailto:".$_POST['Email'].">" . $_POST['Email']. "</a>";
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer();
 
@@ -27,7 +27,7 @@ $mail->Password     = "hi123456gor";                       // Senha do e-mail de
 $mail->From         = $_POST["Email"];                  // E-mail remetente
 $mail->FromName     = $_POST["Nome"];                   // Nome do Remetente
 
-$mail->addAddress("higorturetta@hotmail.com", "Higor Turetta");    // E-mail Destinatário
+$mail->addAddress("contatolitterae.arte@gmail.com", "Litterae - Orçamento");    // E-mail Destinatário
 $mail->isHTML(true);                                   // formato do texto de saída
 $mail->Subject     = "Orçamento Solicitado";              // assunto (título do e-mail)
 $mail->Body        = $mensagem;          // Corpo do e-mail em HTML (destinado ao texto geral)
