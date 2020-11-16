69f2f0f0faec3fbcf96e4b013b69bbbe4b6bddd0
Higor Turetta <higorturetta@hotmail.com>
2020-11-16 15:04:44 -0300

    StyleChanges

--- site/comuns/cabecalho.php
+++ site/comuns/cabecalho.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="<?= SITE_URL . "assets/css/style.css?v=1.52" ?>">
+    <link rel="stylesheet" href="<?= SITE_URL . "assets/css/style.css?v=1.53" ?>">
     <link rel="stylesheet" href="<?= SITE_URL . "assets/css/bootstrap.min.css" ?>">   
     <link rel="stylesheet" href="<?= SITE_URL . "assets/util/dataTables/datatables.min.css" ?>">
 
